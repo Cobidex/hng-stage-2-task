f8e4710d8aa17e420369dd03ba030d08f8266718
Stephen <stevobialor@gmail.com~>
2024-07-07 02:54:25 +0100

    modify config for deployement

--- config.js
+++ config.js
@@ -4,9 +4,10 @@ const { config } = require('dotenv');
 config({ path: './.env'});
 
 
-const { DATABASE_URL, DB_DIALECT } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   logging: false,
 });
