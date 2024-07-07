fafe10c3d698697ae297355eded04700803317e1
Stephen <stevobialor@gmail.com~>
2024-07-07 08:46:39 +0100

    modify codebase

--- controllers/organisationController.js
+++ controllers/organisationController.js
@@ -1,56 +1,44 @@
+const { Op } = require("sequelize");
 const Organisation = require("../models/organisationModel");
 const User = require("../models/userModel.js");
 
 const getOrganisation = async (req, res) => {
   try {
     const { orgId } = req.params;
-    const me = req.user;
-    let org = await Organisation.findOne({
+    const { userId } = req.user.toJSON();
+
+    const org = await Organisation.findOne({
       where: { orgId },
-      include: User,
+      include: {
+        model: User,
+        attributes: [],
+        through: { attributes: [] },
+        where: { userId },
+        required: true,
+      },
     });
 
     if (!org)
-      return res.status(400).json({
-        status: "Bad Request",
-        message: "Client error",
-        statusCode: 400,
+      return res.status(404).json({
+        status: "Not Found",
+        message: "Organisation id not exist or you dont have access to it",
       });
 
-    const { Users } = org.toJSON();
-
-    Users.forEach((user) => {
-      if (user.userId === me.userId) {
-        const filtered = org.toJSON().Users.map((u) => {
-          delete u.password;
-          delete u.UserOrganisation
-          return u;
-        });
-        org = org.toJSON();
-        org.Users = filtered;
-        return res.status(200).json({
-          status: "success",
-          message: "Organisation retrieved",
-          data: org,
-        });
-      }
-    });
-
     return res.status(200).json({
-      status: "Bad Request",
-      message: "Client error",
-      statusCode: 400,
+      status: "success",
+      message: "Organisation data",
+      data: org.toJSON(),
     });
   } catch (e) {
     console.log(e);
-    return res.status(500).json({ error: "internal server error" })
+    return res.status(500).json({ error: "internal server error" });
   }
 };
 
 const createOrganisation = async (req, res) => {
   try {
     const org = await Organisation.create({ ...req.body });
-    await org.addUser(req.user, { through: { selfGranted: false } });
+    await org.addUser(req.user);
     return res.status(201).json({
       status: "success",
       message: "Organisation created successfully",
@@ -68,16 +56,15 @@ const createOrganisation = async (req, res) => {
 
 const getUserOrganisations = async (req, res) => {
   try {
-    const user = req.user.toJSON();
-    const { Organisations } = await User.findOne({
-      where: { userId: user.userId },
-      include: Organisation,
-    });
-
-    const organisations = Organisations.map((org) => {
-      const obj = org.toJSON();
-      delete obj.UserOrganisation;
-      return obj;
+    const { userId } = req.user.toJSON();
+    const organisations = await Organisation.findAll({
+      include: {
+        model: User,
+        attributes: [],
+        through: { attributes: [] },
+        where: { userId },
+        required: true,
+      },
     });
     return res.status(200).json({
       status: "success",
@@ -88,7 +75,7 @@ const getUserOrganisations = async (req, res) => {
     });
   } catch (e) {
     console.log(e);
-    return res.status(500).json({ error: "Internal server error" })
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -99,35 +86,42 @@ const addUserToOrganisation = async (req, res) => {
 
     const user = await User.findOne({ where: { userId } });
 
+    if (!user)
+      return res
+        .status(400)
+        .json({ status: "unsuccessful", message: "user does not exist" });
+
     const organisation = await Organisation.findOne({
       where: { orgId },
-      include: User,
+      include: {
+        model: User,
+        attributes: [],
+        where: {
+          userId: {
+            [Op.not]: userId,
+          },
+        },
+        through: { attributes: [] },
+        required: true,
+      },
     });
 
     if (!organisation)
       return res.status(404).json({
         status: "Not found",
-        message: "organisation not found",
+        message: "organisation not found or user already belongs to it",
         statusCode: 400,
       });
 
-    const { Users } = organisation.toJSON();
-
-    Users.forEach((user) => {
-      if (user.userId === userId)
-        return res.status(400).json({
-          status: "Bad Request",
-          message: "User already belongs to this organisation",
-          statusCode: 400,
-        });
-    });
-
-    await organisation.addUser(user, { through: { selfGranted: false } });
+    await organisation.addUser(user);
     return res.status(200).json({
       status: "success",
       message: "User added to organisation successfully",
     });
-  } catch (e) {}
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "internal server error" });
+  }
 };
 
 module.exports = {
