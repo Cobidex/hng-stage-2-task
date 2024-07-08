9d52601d2753e6a87982681fb8d23c07dfae11c6
Stephen <stevobialor@gmail.com~>
2024-07-08 22:30:29 +0100

    done

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const { accesstoken } = req.headers;
+    const accesstoken = req.headers.authorization.split(' ')[1];
     const { userId } = verifyToken(accesstoken);
     const user = await User.findOne({ where: { userId } });
     req.user = user;
